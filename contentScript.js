c6d96488c3df25f4678de251b8f3023611ae1097
lirik <kirill.isachenko@gmail.com>
2023-06-13 17:24:20 +0700

    fix

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,48 @@
 console.log("content script injected")
 
+// Functions
+
+const GiveMetheChild = (color, msg) => {
+    const errorLabel = document.createElement("div");
+    errorLabel.setAttribute("class", "errorLabel");
+    errorLabel.style.backgroundColor = color;
+    errorLabel.innerText = msg;
+
+    mainCont.appendChild(errorLabel);
+    setTimeout(() => {
+        mainCont.removeChild(errorLabel);
+    }, 2000);
+}
+
+function hexToRgb(hex) {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
+
+function isLightColor(color) {
+    const rgb = hexToRgb(color);
+    const brightness = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
+    return brightness > 128;
+};
+
+const storeColor = (color) => {
+    chrome.storage.local.get("color_hex_code", (resp) => {
+        if (resp.color_hex_code && resp.color_hex_code.length > 0) {
+            resp.color_hex_code.unshift(color);
+            chrome.storage.local.set({ "color_hex_code": resp.color_hex_code });
+        } else {
+            chrome.storage.local.set({ "color_hex_code": [color] });
+        }
+        chrome.runtime.sendMessage({ color: color });
+    });
+}
+
+// Event handlers
+
 chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.from === "popup" && message.query === "eye_dropper_clicked") {
 
@@ -9,101 +52,62 @@ chrome.runtime.onMessage.addListener((message, sender) => {
 
             eyeDropper.open().then(result => {
                 const color = result.sRGBHex;
-                // Copy the color to the clipboard
-                navigator.clipboard.writeText(color).then(function() {
-                    // Success feedback
+                navigator.clipboard.writeText(color).then(() => {
                     console.log('Color copied to clipboard successfully!');
-                }, function(err) {
-                    // Handle errors
+                }, (err) => {
                     console.error('Could not copy color: ', err);
                 });
 
-                chrome.storage.local.get("color_hex_code", (resp) => {
-                    if (resp.color_hex_code && resp.color_hex_code.length > 0) {
-                        resp.color_hex_code.unshift(color);
-                        chrome.storage.local.set({ "color_hex_code": resp.color_hex_code })
-                    }
-                    else {
-                        chrome.storage.local.set({ "color_hex_code": [color] })
-                    }
-                    // Send a message to the background script to change the badge color
-                    chrome.runtime.sendMessage({color: color});
-                })
+                storeColor(color);
+
             }).catch(e => {
-                console.log(e)
-            })
+                console.log(e);
+            });
         }, 500);
     }
-})
+});
 
 window.addEventListener('DOMContentLoaded', () => {
     const mainCont = document.getElementById("mainCont");
     const buttonCont = document.getElementById("picker_btn_cont");
     const resultList = document.getElementById("result");
 
-    const GiveMetheChild = (color, msg) => {
-        const errorLabel = document.createElement("div")
-        errorLabel.setAttribute("class", "errorLabel")
-        errorLabel.style.backgroundColor = color
-        errorLabel.innerText = msg
-
-        mainCont.appendChild(errorLabel)
-        setTimeout(() => {
-            mainCont.removeChild(errorLabel)
-        }, 2000)
-    }
-
-    function hexToRgb(hex) {
-        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
-    };
-
-    function isLightColor(color) {
-        const rgb = hexToRgb(color);
-        const brightness = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
-        return brightness > 128;
-    };
-
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        const tab = tabs[0]
+        const tab = tabs[0];
 
         if (tab.url === undefined || tab.url.indexOf('chrome') == 0) {
-            buttonCont.innerHTML = '<i>ColorPicker can\'t access Chrome pages</i>'
+            buttonCont.innerHTML = '<i>ColorPicker can\'t access Chrome pages</i>';
         }
         else if (tab.url.indexOf('file') === 0) {
-            buttonCont.innerHTML = '<i>ColorPicker can\'t access local pages</i>'
+            buttonCont.innerHTML = '<i>ColorPicker can\'t access local pages</i>';
         } else {
-            const button = document.createElement("button")
-            button.setAttribute("id", "picker_btn")
-            button.innerText = "Pick a color"
+            const button = document.createElement("button");
+            button.setAttribute("id", "picker_btn");
+            button.innerText = "Pick a color";
 
             button.addEventListener("click", () => {
                 if (!window.EyeDropper) {
-                    GiveMetheChild("#FEF2CE", 'Your browser does not support the ColorPicker API')
-                    return
+                    GiveMetheChild("#FEF2CE", 'Your browser does not support the ColorPicker API');
+                    return;
                 }
 
                 chrome.tabs.sendMessage(
                     tabs[0].id,
                     { from: "popup", query: "eye_dropper_clicked" }
                 );
-                window.close()
-            })
+                window.close();
+            });
 
-            buttonCont.appendChild(button)
+            buttonCont.appendChild(button);
         }
     });
 
     chrome.storage.local.get("color_hex_code", (resp) => {
         if (resp.color_hex_code && resp.color_hex_code.length > 0) {
             resp.color_hex_code.reverse().forEach(hexCode => {
-                const liElem = document.createElement("span")
-                liElem.innerText = hexCode
-                liElem.style.backgroundColor = hexCode
+                const liElem = document.createElement("span");
+                liElem.innerText = hexCode;
+                liElem.style.backgroundColor = hexCode;
                 if (isLightColor(hexCode)) {
                     liElem.style.color = '#000';
                 } else {
@@ -111,19 +115,19 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
                 liElem.addEventListener("click", () => {
                     navigator.clipboard.writeText(hexCode);
-                    GiveMetheChild("#FEF2CE", "Hex code is copied to clipboard!")
-                })
-                resultList.prepend(liElem)
-            })
+                    GiveMetheChild("#FEF2CE", "Hex code is copied to clipboard!");
+                });
+                resultList.prepend(liElem);
+            });
 
-            const ClearButton = document.createElement("button")
-            ClearButton.innerText = "Clear colors"
-            ClearButton.setAttribute("id", "ClearButton")
+            const ClearButton = document.createElement("button");
+            ClearButton.innerText = "Clear colors";
+            ClearButton.setAttribute("id", "ClearButton");
             ClearButton.addEventListener("click", () => {
-                chrome.storage.local.remove("color_hex_code")
-                window.close()
-            })
-            mainCont.appendChild(ClearButton)
+                chrome.storage.local.remove("color_hex_code");
+                window.close();
+            });
+            mainCont.appendChild(ClearButton);
         }
-    })
-})
+    });
+});
